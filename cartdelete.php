db6f900f67726d06219403dda61b76930510920f
Khang-Le07 <letrannguyenkhang2001@gmail.com>
2021-11-29 16:33:55 +0700

    hihihi de vl

--- cartdelete.php
+++ cartdelete.php
@@ -1,8 +1,9 @@
 <?php
-    $id = $_GET['productid'];
+include 'connect.php';
+include 'loading.php';
+    $id = $_GET['id'];
     $user = $_SESSION['user'];
     $sql = "DELETE  FROM cartproduct WHERE productid = $id and userid = $user[id]" ;
     $query = mysqli_query($conn, $sql);
-    // header('location: index.php');
-    var_dump($id)
+    header('location: index.php');
 ?>
